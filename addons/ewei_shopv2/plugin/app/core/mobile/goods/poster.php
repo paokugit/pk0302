1588789bd6b116a22234e61dea5d6e4a557508fe
livewen <1406385845@qq.com>
2019-03-18 11:28:46 +0800

    文件丢失

--- addons/ewei_shopv2/plugin/app/core/mobile/goods/poster.php
+++ addons/ewei_shopv2/plugin/app/core/mobile/goods/poster.php
@@ -334,6 +334,7 @@ class Poster_EweiShopV2Page extends AppMobilePage
 
     public function gethelpimage()
     {
+        
         $member = $this->member;
         if( empty($member) )
         {
