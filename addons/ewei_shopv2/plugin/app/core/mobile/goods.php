04b797385686ffa828869ee2f4d10cd9423c1748
livewen <1406385845@qq.com>
2019-04-22 11:23:27 +0800

    不同会员级别可购买

--- addons/ewei_shopv2/plugin/app/core/mobile/goods.php
+++ addons/ewei_shopv2/plugin/app/core/mobile/goods.php
@@ -312,7 +312,7 @@ class Goods_EweiShopV2Page extends AppMobilePage
 				$goods["cannotbuy"] = "超出最大购买数量";
 			}
 		}
-		$levelid = $member["level"];
+		$levelid = $member["agentlevel"];
 		$groupid = $member["groupid"];
 		$goods["levelbuy"] = "1";
 		if( $goods["buylevels"] != "" ) 
@@ -322,7 +322,7 @@ class Goods_EweiShopV2Page extends AppMobilePage
 			{
 				$goods["levelbuy"] = 0;
 				$goods["canbuy"] = 0;
-				$goods["cannotbuy"] = "购买级别不够";
+				$goods["cannotbuy"] = $this->canByLevels($buylevels);
 			}
 		}
 		$goods["groupbuy"] = "1";
@@ -1952,6 +1952,19 @@ class Goods_EweiShopV2Page extends AppMobilePage
             default:
                 return '';
         }
+    }
+
+    /**
+     * 可购买的会员级别
+     * @param $cids
+     */
+    public function canByLevels($cids){
+        if(count($cids)>1 && in_array(1,$cids)) return "健康达人以上级别专享";
+        if(count($cids)==1 && in_array(1,$cids)) return "健康达人专享";
+        if(count($cids)==1 && in_array(2,$cids)) return "星选达人专享";
+        if(count($cids)==1 && in_array(5,$cids)) return "店主专享";
+        if(count($cids)>1 && in_array(2,$cids)) return "星选达人以上级别专享";
+        return "您当前会员等级没有购买权限";
     }
 }
 ?>
