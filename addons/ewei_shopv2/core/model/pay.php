538736bd369b340bcf894ffc2af3bc9be3885a79
hcc <1452210175@qq.com>
2019-05-27 18:31:01 +0800

    提交Git  最近几天

--- addons/ewei_shopv2/core/model/pay.php
+++ addons/ewei_shopv2/core/model/pay.php
@@ -16,6 +16,49 @@ class Pay_EweiShopV2Model
 
 		return error(-1, '没有全付通支付!');
 	}
+
+	/**
+	 * 小程序支付
+	 * @param $data
+	 * @return mixed
+	 */
+	public function pay($data)
+	{
+		global $_W;
+		$config = pdo_fetch('select * from '.tablename('ewei_shop_payment').' where id=:id and uniacid=:uniacid',[':id'=>1,':uniacid'=>$_W['uniacid']]);
+		$params = [];
+		$params['appid'] = $config['sub_appid'];
+		$params['mch_id'] = $config['sub_mch_id'];
+		$params['nonce_str'] = $data['random'];
+		$params['out_trade_no'] = time();
+		$params['total_fee'] = $data['money'] * 100;
+		$params['body'] = $data['body'];
+		$params['spbill_create_ip'] = $data['ip'];
+		$params['trade_type'] = 'JSAPI';
+		$params['notify_url'] = $data['url'];
+		$params['openid'] = $data['openid'];
+		ksort($params, SORT_STRING);
+		$string1 = "";
+		foreach( $params as $key => $v )
+		{
+			if( empty($v) )
+			{
+				continue;
+			}
+			$string1 .= (string) $key . "=" . $v . "&";
+		}
+		$string1 .= "key=" . $config["apikey"];
+		$params["sign"] = strtoupper(md5(trim($string1)));    //签名
+		$dat = array2xml($params);
+		$response = ihttp_request("https://api.mch.weixin.qq.com/pay/unifiedorder", $dat);
+		if( is_error($response) )
+		{
+			return $response;
+		}
+		$xml = simplexml_load_string(trim($response["content"]), "SimpleXMLElement", LIBXML_NOCDATA);
+		$result = json_decode(json_encode($xml), true);
+		return $result;
+	}
 }
 
 if (!defined('IN_IA')) {
--- addons/ewei_shopv2/core/model/qrcode.php
+++ addons/ewei_shopv2/core/model/qrcode.php
@@ -90,6 +90,46 @@ class Qrcode_EweiShopV2Model
 
 		return $_W['siteroot'] . 'addons/ewei_shopv2/data/qrcode/' . $_W['uniacid'] . '/' . $file;
 	}
+
+	/**
+	 * 商城收款二维码
+	 * @param int $mid
+	 * @param int $r
+	 * @param int $posterid
+	 * @return string
+	 */
+	public function createSQrcode($mid = 0, $r = 0,$posterid = 0)
+	{
+		global $_W;
+		global $_GPC;
+		$path = IA_ROOT . '/addons/ewei_shopv2/data/qrcode/' . $_W['uniacid'] . '/';
+		if (!is_dir($path)) {
+			load()->func('file');
+			mkdirs($path);
+		}
+		//设置二维码的URL路径
+		if(!empty($r)){
+			$url = mobileUrl($r, array('mid' => $mid), true);
+		}
+		if (!empty($posterid)) {
+			$url .= '&posterid=' . $posterid;
+		}
+		//生成二维码
+		$file = 'shop_qrcode_' . $posterid . '_' . $mid . '.png';
+		$qrcode_file = $path . $file;
+		if (!is_file($qrcode_file)) {
+			require_once IA_ROOT . '/framework/library/qrcode/phpqrcode.php';
+			QRcode::png($url, $qrcode_file, QR_ECLEVEL_L, 4);
+		}
+		//设置二维码的路径
+		$qrcode = $_W['siteroot'] . 'addons/ewei_shopv2/data/qrcode/' . $_W['uniacid'] . '/' . $file;
+		//把二维码放在设定好的背景图里面  $logo二维码的背景图   imagecopyresampled 设置二维码在背景图的位置
+		$logo = "";
+		imagecopyresampled($logo,$qrcode,555,333,0,0,168,168,imagesx($qrcode), imagesy($qrcode));
+		//输出图片
+		imagepng($logo, $qrcode);
+		return $qrcode;
+	}
 }
 
 if (!defined('IN_IA')) {
