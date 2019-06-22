c8c0d4b706cf009992cc2ae56108dc487f400b61
hcc <1452210175@qq.com>
2019-06-22 09:00:23 +0800

    代码的完善

--- addons/ewei_shopv2/core/model/game.php
+++ addons/ewei_shopv2/core/model/game.php
@@ -18,44 +18,22 @@ class Game_EweiShopV2Model{
                 $array[$val['reward'.($key+1)]] = $val['rate'.($key+1)] *100;
             }
             asort($array);
+            //计算总额
             $num = array_sum($array);
             $rand = rand(1,$num);
-            $add2 = [
-                'openid'=>$openid,
-                'type'=>$type,
-                'module'=>'ewei_shopv2',
-                'num'=>$money==0?0:-$money,
-                'uniacid'=>$_W['uniacid'],
-                'createtime'=>time(),
-                'remark'=>'幸运转盘抽奖',
-            ];
-            if($data['type'] == 1){
-                $add2['credittytpe'] = "credit1";
-            }elseif ($data['type'] == 2){
-                $add2['credittype'] = "credit3";
-            }
-
+            //扣除抽奖的钱的日志
+            $this->addlog($openid,$type,-$money,$data['type'],'幸运转盘抽奖');
             //如果不是免费抽奖  就减去对应的东西
             if($money != 0){
-                $this->addcredit($openid,-$money,$type);
+                $this->addcredit($openid,-$money,$data['type']);
             }
-            pdo_insert('mc_credits_record',$add2);
-            pdo_insert('ewei_shop_member_credit_record',$add2);
             foreach ($array as $key=>$value){
                 if($rand <= $value){
                     preg_match('/\d+/',$key,$arr);
-                    $add1 = [
-                        'num'=>$arr[0],
-                        'type'=>1,
-                        'reward'=>"抽中".$key,
-                        'createtime'=>time(),
-                    ];
-                    //array_merge($a,$b)  当$a,$b有共同的键  后者后覆盖前者
-                    $add = array_merge($add2,$add1);
+                    //添加中奖日志
+                    $this->addlog($openid,1,$arr[0],$data['type'],"抽中".$key);
                     //如果$data['type']  == 1 就是卡路里   == 2 就是折扣宝
-                    $this->addcredit($openid,$arr[0],$type);
-                    pdo_insert('mc_credits_record',$add);
-                    pdo_insert('ewei_shop_member_credit_record',$add);
+                    $this->addcredit($openid,$arr[0],$data['type']);
                     return $key;
                     break;
                 }else{
@@ -79,5 +57,34 @@ class Game_EweiShopV2Model{
               $credit = $member['credit3'] + $money;
               pdo_update('ewei_shop_member',['credit3'=>$credit],['openid'=>$openid]);
           }
+      }
+
+    /**
+     * 添加日志
+     * @param $openid
+     * @param $type
+     * @param $money
+     * @param $datatype
+     * @param $remark
+     */
+      public function addlog($openid,$type,$money,$datatype,$remark)
+      {
+          global $_W;
+          $add = [
+              'openid'=>$openid,
+              'type'=>$type,
+              'module'=>'ewei_shopv2',
+              'num'=>$money==0?0:$money,
+              'uniacid'=>$_W['uniacid'],
+              'createtime'=>time(),
+              'remark'=>$remark,
+          ];
+          if($datatype == 1){
+              $add['credittytpe'] = "credit1";
+          }elseif ($datatype == 2){
+              $add['credittype'] = "credit3";
+          }
+          pdo_insert('mc_credits_record',$add);
+          pdo_insert('ewei_shop_member_credit_record',$add);
       }
 }
